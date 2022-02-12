7af56a5f805f37c3d58f74699743890ef2893441
Junio Fernandes <prof.juniofernandes@gmail.com>
2022-02-12 17:50:50 -0300

    Projeto Iniciado

---
+++ src/main/java/com/ifpesalgueiro/cursofic/CursoFicApplication.java
@@ -0,0 +1,13 @@
+package com.ifpesalgueiro.cursofic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoFicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoFicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpesalgueiro/cursofic/CursoFicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpesalgueiro.cursofic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoFicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
